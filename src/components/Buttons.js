30b0d283f9c7a075bb5990e9d0c9ed5c811567e8
sdale0321 <67200326+sdale0321@users.noreply.github.com>
2023-08-14 19:26:45 -0500

    completed assignment

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,21 @@
-const Buttons = () => {
+import satData from "./satData";
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
+
+  //code continues
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
